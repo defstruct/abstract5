ef75125f71ded3b8857e7c52e281708b2188f9fe
Jong-won Choi <jong-won.choi@defstruct.com>
2011-12-13 21:20:05 +1100

    Simplified config sample. Developing idea about multisite, http-request-handler, etc

--- src/abstract5.lisp
+++ src/abstract5.lisp
@@ -18,12 +18,84 @@
   "$Id$
    Report bugs to: jongwon.choi@internode.on.net")
 
+(defparameter *abstract5-home-dir* (merge-pathnames "../" *load-pathname*)
+  "Pathname for the 'abstract5/site-instances/defstruct'")
+
 ;;;
-;;; Server start
+;;; Multi-site support
 ;;;
-(defun start-abstract5 ()
-  (let ((hunchentoot:*dispatch-table*
-  )
+(defparameter *multi-site-instances* ()
+  "List of all multi-site instances. Used in dispatching reqeust, selecting DB schema, choosing site home, etc")
+
+(defstruct abstract5-site
+  (:name	nil	:type symbol)
+  (:domains	()	:type list)
+  (:db-schema	""	:type string)
+  (:home	#P""	:type pathname))
+
+(defun get-config-parameters (def keys)
+  (mapcar (lambda (key)
+	    (getf def key))
+	  keys))
+
+(defparameter *site-paths*
+  ;; FIXME: not finalised yet
+  (mapcar #'make-pathname '("packages" "themes")))
+
+(defun ensure-site-directories-exist (site-home)
+  (dolist (path *site-paths*)
+    (ensure-directories-exist (merge-pathnames path site-home))))
+
+(defun make-site-instance (site-def)
+  (destructuring-bind (name domains)
+      (get-config-parameters site-def '(:name :domains))
+    (assert (symbolp name) () "Given name ~S is not a symbol" name)
+    (assert (and (listp domains) (every #'stringp domains))
+	    ()
+	    "Given domains ~S must be list of strings" domains)
+    (assert (not (find name *multi-site-instances* :key abstract5-site-name))
+	    ()
+	    "Site ~S exists" name)
+
+    ;; qouted schema name to avoid invalid DB names
+    (let ((db-schema (format nil "\"~A\"" name))
+	  (home (merge-pathnames (make-pathname :directory  (format nil "~(~A~)/" name))
+				   *abstract5-home-dir*)))
+      (ensure-site-directories-exist home)
+
+      (make-abstract5-site :name name :domains domains :db-schema db-schema :home home))))
+
+(defun build-multi-site-instances (site-defs)
+  (setf *multi-site-instances* (mapcar #'make-site-instance site-defs)))
+
+;;; Dynamic variables for site namespace
+(defvar *site-database-schema*)
+(defvar *site-home-dir*)
+
+(defmacro with-site-context ((domain) &body body)
+  (let ((site (gensym "site")))
+    `(let ((,site (find ,domain *multi-site-instances*
+			:key #'abstract5-site-domains
+			:test (curry-right #'member :test #'string=))))
+       (let ((*site-database-schema* (abstract5-site-db-schema ,site))
+	     (*site-home-dir*	     (abstract5-site-home ,site)))
+	 (progn
+	   ,@body)))))
+
+
+
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun http-request-handler (request)
+  (with-site-context ((hunchentoot:host request))
+    ;; check maintenance?
+    ;; ...
+    (multiple-value-bind (model view controller)
+	;; FIXME:
+	(get-mvc-from-uri FIXME)
+      (render-html view (get-view-environment controller)))))
 
 (defun main ()
   ;; check config file. Load it or start installation web page
@@ -34,12 +106,14 @@
   ;; timezone, session,
   )
 
-(defun handler ()
-  ;; check maintenance?
-  ;; ...
-  (multiple-value-bind (model view controller)
-      ;; FIXME:
-      (get-mvc-from-uri FIXME)
-    (render-html view (get-view-environment controller))))
+
+;;;
+;;; Replace Hunchentoot's dispatcher function
+;;;
+(in-package :hunchentoot)
+
+(defun list-request-dispatcher (request)
+  (declare (ignore request))
+  #'abstract5::http-request-handler)
 
 ;;; ABSTRACT5.LISP ends here
