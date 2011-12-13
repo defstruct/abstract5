3bc7438151ab532bfada57f470c832cf2cc0ed4b
Jong-won Choi <jong-won.choi@defstruct.com>
2011-12-13 22:54:16 +1100

    Added a new file to generate class/schema

---
+++ src/class-generator.lisp
@@ -0,0 +1,21 @@
+;;;;   -*- Mode: lisp; Package: abstract5; Syntax: Common-lisp; encoding: utf-8; -*-
+;;
+;; Copyright (C) 2011 Jong-won Choi
+;; All rights reserved.
+;;
+;; Author:  $Author$
+;; Version: $Id$
+;;
+;;;; Commentary:
+;;
+;;	Generate persistent class definitions/SQL schema from concrete5 db.xml files.
+;;
+;;;; Code:
+
+(in-package :abstract5)
+
+(defconstant +class-generator-version+ "$Revision$"
+  "$Id$
+   Report bugs to: jongwon.choi@internode.on.net")
+
+;;; CLASS-GENERATOR.LISP ends here
