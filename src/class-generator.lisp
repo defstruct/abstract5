b469dcf6266ee5585d2fe4b8d227897940029bf5
Jong-won Choi <jong-won.choi@defstruct.com>
2011-12-14 15:44:30 +1100

    Added parsing db.xml -> lisp sexp

--- src/class-generator.lisp
+++ src/class-generator.lisp
@@ -8,7 +8,7 @@
 ;;
 ;;;; Commentary:
 ;;
-;;	Generate persistent class definitions/SQL schema from concrete5 db.xml files.
+;;	Generate persistent class definitions/SQL schema from concrete5 axmls files.
 ;;
 ;;;; Code:
 
@@ -18,4 +18,41 @@
   "$Id$
    Report bugs to: jongwon.choi@internode.on.net")
 
+(asdf:load-system :s-xml :verbose t)
+(use-package :s-xml)
+
+(defun axmls-new-element-hook (name attributes seed)
+  (cond ((member name '(table field key unsigned index default notnull autoincrement unique col
+			deftimestamp opt fulltext
+			;;automatic auto autoincrement primary def
+			;;  defdate deftimestamp noquote constraints
+			)
+		 :test #'string-equal)
+	 `(,(intern (string-upcase name) :keyword)
+	    ,@(mapcan (lambda (pair)
+			`(,(intern (string-upcase (car pair)) :keyword)
+			   ,(cdr pair)))
+		      attributes)))
+	((member name '(schema) :test #'string-equal)
+	 nil)
+	(t (error "Unknown ~S ~S" name attributes))))
+
+(defun axmls-finish-element-hook (name attributes parent-seed seed)
+  (when seed
+    (if (string-equal name 'table)
+	(push seed parent-seed)
+	(append parent-seed (list seed)))))
+
+(defun axmls-text-hook (string seed)
+  (unless (whitespace-string? string)
+    (list string)))
+
+(defun axmls->lsexp (in)
+  "Parse and trace a toplevel XML element from stream in"
+  (nreverse (first (s-xml:start-parse-xml in
+					  (make-instance 's-xml:xml-parser-state
+							 :seed (list)
+							 :new-element-hook #'axmls-new-element-hook
+							 :finish-element-hook #'axmls-finish-element-hook
+							 :text-hook #'axmls-text-hook)))))
 ;;; CLASS-GENERATOR.LISP ends here
