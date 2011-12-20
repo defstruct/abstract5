6f7cce87565340d548df06bb7395197bd895ea15
Jong-won Choi <jong-won.choi@defstruct.com>
2011-12-20 16:55:13 +1100

    Minor changes

--- site-instances/root/mvc/login.lisp
+++ site-instances/root/mvc/login.lisp
@@ -2,9 +2,32 @@
 ;;
 ;; Copyright (C) 2011 Jong-won Choi
 ;; All rights reserved.
+;; Distributed under the BSD-style license:
+;; http://www.opensource.org/licenses/bsd-license.php
 ;;
-;; Author:  $Author$
-;; Version: $Id$
+;; Redistribution and use in source and binary forms, with or without
+;; modification, are permitted provided that the following conditions
+;; are met:
+
+;;   * Redistributions of source code must retain the above copyright
+;;     notice, this list of conditions and the following disclaimer.
+
+;;   * Redistributions in binary form must reproduce the above
+;;     copyright notice, this list of conditions and the following
+;;     disclaimer in the documentation and/or other materials
+;;     provided with the distribution.
+
+;; THIS SOFTWARE IS PROVIDED BY THE AUTHOR 'AS IS' AND ANY EXPRESSED
+;; OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+;; WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+;; ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+;; DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+;; DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+;; GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+;; INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+;; WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+;; NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+;; SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ;;
 ;;;; Commentary:
 ;;
@@ -19,11 +42,13 @@
    Report bugs to: jongwon.choi@internode.on.net")
 
 (define-mvc login () ;; without :site, all sites share this definition
-  :view ((:uri  "/login.html")
+  :view ((:uri  "/login")
 	 (:html "html-views/login.html"))
   :controller ((:methods ((do-login (mvc operator
-					 (user-name :user-name :string) (password :password :string)
+					 (user-name :user-name :string)
+					 (password :password :string)
 					 (maintain-login :maintain-login :boolean)
+					 (email :email :string)
 					 ;; FIXME: :list vs :ordered-list
 					 (attributes :user-attributes :list))
 			    (when (banned-ip (hunchentoot:real-remote-addr))
@@ -82,6 +107,5 @@
    (:user-invalid :use-email "Invalid email address or password.")
    (:user-invalid :use-user-name "Invalid username or password.")
    (:user-inactive "This user is inactive. Please contact us regarding this account.")))
-
 
 ;;; LOGIN.LISP ends here
