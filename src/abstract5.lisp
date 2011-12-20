217da50fd2df10985bcb2256949b367004ba1f99
Jong-won Choi <jong-won.choi@defstruct.com>
2011-12-20 16:54:12 +1100

    An example template variable list

--- src/abstract5.lisp
+++ src/abstract5.lisp
@@ -2,9 +2,32 @@
 ;;
 ;; Copyright (C) 2011 Jong-won Choi
 ;; All rights reserved.
+;; Distributed under the BSD-style license:
+;; http://www.opensource.org/licenses/bsd-license.php
 ;;
-;; Author:  $Author$
-;; Version: $Id$
+;; Redistribution and use in source and binary forms, with or without
+;; modification, are permitted provided that the following conditions
+;; are met:
+
+;;   * Redistributions of source code must retain the above copyright
+;;     notice, this list of conditions and the following disclaimer.
+
+;;   * Redistributions in binary form must reproduce the above
+;;     copyright notice, this list of conditions and the following
+;;     disclaimer in the documentation and/or other materials
+;;     provided with the distribution.
+
+;; THIS SOFTWARE IS PROVIDED BY THE AUTHOR 'AS IS' AND ANY EXPRESSED
+;; OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+;; WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+;; ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+;; DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+;; DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+;; GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+;; INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+;; WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+;; NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+;; SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ;;
 ;;;; Commentary:
 ;;
@@ -124,10 +147,13 @@
 			 :theme-js-files ((:js-file "/site-instances/root/themes/core/js/jquery.js")
 					  (:js-file "/site-instances/root/themes/core/js/ccm.base.js"))
 			 ))
-    :theme-html-body ((path1 :var "xxx")
-		      (path2 :var "yyy"))
-    :theme-html-footer ((path1 :var "xxx")
-			(path2 :var "yyy"))
+    :theme-html-body (("site-instances/root/themes/core/body-view.html"
+		       ;; FIXME: establish site based css, img, etc directories
+		       :site-logo "/a/b/c/logo.png"
+		       :site-logo-alt ()
+		       :errors ())
+		      ("site-instances/root/mvc/html-views/plain-login.html"
+		       :site-name "example-site"))
     :theme-css-files ((:css-file "ccm.default.theme.css"))
 	      ;; no :extra-header
     :mvc-errors (get-mvc-errors)))
